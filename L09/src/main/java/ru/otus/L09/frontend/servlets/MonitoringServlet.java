0ab80303a39b7a548cd56fb18ee09578af98debc
Дмитрий Звягин (Dmitriy Zvyagin) <dzvyagin@iitdgroup.ru>
2017-07-18 18:14:09 +0300

    commit

--- L09/src/main/java/ru/otus/L09/frontend/servlets/MonitoringServlet.java
+++ L09/src/main/java/ru/otus/L09/frontend/servlets/MonitoringServlet.java
@@ -16,6 +16,7 @@ import java.util.Map;
 public class MonitoringServlet extends HttpServlet {
 
 
+    @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
 
--- L09/src/main/java/ru/otus/L09/frontend/servlets/ORMOperationsServlet.java
+++ L09/src/main/java/ru/otus/L09/frontend/servlets/ORMOperationsServlet.java
@@ -1,7 +1,30 @@
 package ru.otus.L09.frontend.servlets;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by dzvyagin on 18.07.2017.
  */
-public class ORMOperationsServlet  {
+public class ORMOperationsServlet extends HttpServlet {
+
+    /**
+     * Getting entity
+     */
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    /**
+     * Creating new entity
+     */
+    @Override
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+    }
+
 }
