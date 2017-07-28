5dd91b740dce18a89d496068c8ae8f9f43870e7c
Дмитрий Звягин (Dmitriy Zvyagin) <dzvyagin@iitdgroup.ru>
2017-07-28 17:22:08 +0300

    L15 ctill in progress

--- L15/src/main/java/ru/otus/L15/controllers/AppController.java
+++ L15/src/main/java/ru/otus/L15/controllers/AppController.java
@@ -2,6 +2,7 @@ package ru.otus.L15.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,12 +26,19 @@ public class AppController {
     }
 
     @RequestMapping(value = {"/login"}, method = {RequestMethod.GET, RequestMethod.POST})
-    public String login(@RequestParam String login, HttpServletRequest request, HttpServletResponse response) {
+    public String login(@RequestParam String login, HttpServletRequest request, HttpServletResponse response, Model model) {
         response.addCookie(new Cookie("L15.1-login", login));
         request.getSession().setAttribute("login", login);
-
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("text/html;charset=utf-8");
+        model.addAttribute("login", login);
         System.out.println("HomeController: Passing through...");
-        return "index";
+        return "login";
+    }
+
+    @RequestMapping(value = "/monitoring")
+    public String getMonitoring(){
+        return "monitoring";
     }
 
 
